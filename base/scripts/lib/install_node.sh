5a09cb43fe67cdf314181182d587181fe0ae27c7
Madushan Nishantha <j.l.madushan@gmail.com>
2016-02-06 21:49:43 +0530

    stuff from mhart/alpine-node

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -8,11 +8,22 @@ NODE_ARCH=x64
 NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
 
 cd /tmp
-curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
-tar xzf ${NODE_DIST}.tar.gz
-rm -rf /opt/nodejs
-mkdir -p /opt
-mv ${NODE_DIST} /opt/nodejs
+apk add --update curl make gcc g++ binutils-gold python linux-headers paxctl libgcc libstdc++
 
-ln -sf /opt/nodejs/bin/node /usr/bin/node
-ln -sf /opt/nodejs/bin/npm /usr/bin/npm
+CONFIG_FLAGS="--fully-static --without-npm" DEL_PKGS="libgcc libstdc++" RM_DIRS=/usr/include
+
+curl -sSL https://nodejs.org/dist/${VERSION}/node-${VERSION}.tar.gz | tar -xz
+cd /node-${VERSION}
+./configure --prefix=/usr ${CONFIG_FLAGS}
+make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)
+make install
+paxctl -cm /usr/bin/node
+cd /
+if [ -x /usr/bin/npm ]; then
+    npm install -g npm@${NPM_VERSION}
+    find /usr/lib/node_modules/npm -name test -o -name .bin -type d | xargs rm -rf;
+fi
+apk del curl make gcc g++ binutils-gold python linux-headers paxctl ${DEL_PKGS}
+rm -rf /etc/ssl /node-${VERSION} ${RM_DIRS} \
+   /usr/share/man /tmp/* /var/cache/apk/* /root/.npm /root/.node-gyp \
+   /usr/lib/node_modules/npm/man /usr/lib/node_modules/npm/doc /usr/lib/node_modules/npm/html
