322e3b7e159f35693a3f40e653459eeb96f0f80e
Madushan Nishantha <j.l.madushan@gmail.com>
2016-02-06 09:14:34 +0530

    converted some more bash->sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -1,6 +1,6 @@
 set -e
 
-bash $METEORD_DIR/lib/install_base.sh
-bash $METEORD_DIR/lib/install_node.sh
-bash $METEORD_DIR/lib/install_phantomjs.sh
-bash $METEORD_DIR/lib/cleanup.sh
+sh $METEORD_DIR/lib/install_base.sh
+sh $METEORD_DIR/lib/install_node.sh
+sh $METEORD_DIR/lib/install_phantomjs.sh
+sh $METEORD_DIR/lib/cleanup.sh
