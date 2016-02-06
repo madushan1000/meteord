76ac03c7e6bfd5192e4d48aa489becb76c1ee824
Madushan Nishantha <j.l.madushan@gmail.com>
2016-02-06 21:52:33 +0530

    NODE_VERSION/VERSION

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 NODE_VERSION=0.10.41
+NPM_VERSION=3.0.0
 NODE_ARCH=x64
 
 # check we need to do this or not
@@ -12,8 +13,8 @@ apk add --update curl make gcc g++ binutils-gold python linux-headers paxctl lib
 
 CONFIG_FLAGS="--fully-static --without-npm" DEL_PKGS="libgcc libstdc++" RM_DIRS=/usr/include
 
-curl -sSL https://nodejs.org/dist/${VERSION}/node-${VERSION}.tar.gz | tar -xz
-cd /node-${VERSION}
+curl -sSL https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}.tar.gz | tar -xz
+cd /node-${NODE_VERSION}
 ./configure --prefix=/usr ${CONFIG_FLAGS}
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)
 make install
@@ -24,6 +25,6 @@ if [ -x /usr/bin/npm ]; then
     find /usr/lib/node_modules/npm -name test -o -name .bin -type d | xargs rm -rf;
 fi
 apk del curl make gcc g++ binutils-gold python linux-headers paxctl ${DEL_PKGS}
-rm -rf /etc/ssl /node-${VERSION} ${RM_DIRS} \
+rm -rf /etc/ssl /node-${NODE_VERSION} ${RM_DIRS} \
    /usr/share/man /tmp/* /var/cache/apk/* /root/.npm /root/.node-gyp \
    /usr/lib/node_modules/npm/man /usr/lib/node_modules/npm/doc /usr/lib/node_modules/npm/html
